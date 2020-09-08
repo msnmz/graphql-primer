d1c1bce1a0f4eb13b0dfc3d37f707f5572b59118
msnmz <snmez.muharrem@gmail.com>
2020-09-08 11:31:13 +0200

    Creates sample query

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import Express from 'express';
+import { buildSchema, Resolver, Query } from 'type-graphql';
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return 'Hello world!';
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log('server listening on http://localhost:4000');
+  });
+};
+
+main();
